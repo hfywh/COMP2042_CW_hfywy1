7295163c5a842f163718d5403415ad275e4d26f5
hfywh <hfywy.nottingham.edu.my>
2021-12-01 12:26:37 +0800

    Modify properties of SuperBrick

--- src/test/SuperBrick.java
+++ src/test/SuperBrick.java
@@ -10,7 +10,7 @@ public class SuperBrick extends Brick {
     private static final Color DEF_INNER = new Color(138, 229, 132);
     private static final Color DEF_BORDER = new Color(22, 106, 50);
     private static final int SUPER_STRENGTH = 2;
-    private static final double SUPER_PROBABILITY = 0.2;
+    private static final double SUPER_PROBABILITY = 0.4;
 
     private Random rnd;
     private Crack crack;
