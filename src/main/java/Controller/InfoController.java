6eb034b8be683664caec715d109bc6d23b332eb9
hfywh <hfywy.nottingham.edu.my>
2021-12-09 13:02:11 +0800

    Refactor: Extract method Javadoc: add javadoc

--- src/main/java/Controller/InfoController.java
+++ src/main/java/Controller/InfoController.java
@@ -7,8 +7,16 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+/**
+ * Controller for the info
+ * listen to mouse of the user and give responses
+ */
 public class InfoController implements MouseListener, MouseMotionListener {
 
+    /**
+     * when the back button is clicked, back to home menu
+     * @param mouseEvent mouse clicked
+     */
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
         Point p = mouseEvent.getPoint();
@@ -18,20 +26,28 @@ public class InfoController implements MouseListener, MouseMotionListener {
         }
     }
 
+    /**
+     * set the color of button to gray and color of text to red if any buttons is pressed
+     * @param mouseEvent mouse pressed
+     */
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
         Point p = mouseEvent.getPoint();
         if(InfoModel.getBackButton().contains(p)){
             InfoModel.backClicked = true;
-            GameFrame.getInfoModel().repaint(InfoModel.getBackButton().x,InfoModel.getBackButton().y,InfoModel.getBackButton().width+1,InfoModel.getBackButton().height+1);
+            repaintBackBtn();
         }
     }
 
+    /**
+     * set the color of button and color of text to white if any buttons is not pressed
+     * @param mouseEvent mouse released
+     */
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
         if(InfoModel.backClicked ){
             InfoModel.backClicked = false;
-            GameFrame.getInfoModel().repaint(InfoModel.getBackButton().x,InfoModel.getBackButton().y,InfoModel.getBackButton().width+1,InfoModel.getBackButton().height+1);
+            repaintBackBtn();
         }
     }
 
@@ -51,6 +67,10 @@ public class InfoController implements MouseListener, MouseMotionListener {
 
     }
 
+    /**
+     * set to hand cursor when the cursor is on any buttons
+     * @param mouseEvent mouse moved
+     */
     @Override
     public void mouseMoved(MouseEvent mouseEvent) {
         Point p = mouseEvent.getPoint();
@@ -59,6 +79,13 @@ public class InfoController implements MouseListener, MouseMotionListener {
         else
             GameFrame.getInfoModel().setCursor(Cursor.getDefaultCursor());
 
+    }
+
+    /**
+     * repaint back button
+     */
+    public void repaintBackBtn(){
+        GameFrame.getInfoModel().repaint(InfoModel.getBackButton().x,InfoModel.getBackButton().y,InfoModel.getBackButton().width+1,InfoModel.getBackButton().height+1);
     }
 
 }
